1ce072b04eddfca3c55f75f06d80b4ca2eae70f9
faithcredit <honest.dedicate@gmail.com>
2023-06-30 14:26:21 +0900

    Responding to mouse events in Flutter Desktop

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -9,12 +9,19 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
+  final HttpHelper helper = HttpHelper();
   List<Book> books = [];
+  List<Color> bgColors = [];
+
   @override
   void initState() {
     super.initState();
-    final HttpHelper helper = HttpHelper();
     helper.getBooks('flutter').then((List<Book> value) {
+      int i;
+      for (i = 0; i < value.length; i++) {
+        bgColors.add(Colors.white);
+      }
+
       setState(() {
         books = value;
       });
@@ -35,15 +42,29 @@ class _BookListScreenState extends State<BookListScreen> {
             childAspectRatio: isLargeScreen ? 8 : 5,
             crossAxisCount: isLargeScreen ? 2 : 1,
             children: List.generate(books.length, (index) {
-              return ListTile(
-                title: Text(books[index].title),
-                subtitle: Text(books[index].authors),
-                leading: CircleAvatar(
-                  backgroundImage: (books[index].thumbnail) == ''
-                      ? null
-                      : NetworkImage(books[index].thumbnail),
+              return GestureDetector(
+                onTap: () => setColor(Colors.lightBlue, index),
+                onSecondaryTap: () => setColor(Colors.white, index),
+                onLongPress: () => setColor(Colors.white, index),
+                child: ColoredBox(
+                  color: bgColors.isNotEmpty ? bgColors[index] : Colors.white,
+                  child: ListTile(
+                    title: Text(books[index].title),
+                    subtitle: Text(books[index].authors),
+                    leading: CircleAvatar(
+                      backgroundImage: (books[index].thumbnail) == ''
+                          ? null
+                          : NetworkImage(books[index].thumbnail),
+                    ),
+                  ),
                 ),
               );
             })));
+  }
+
+  void setColor(Color color, int index) {
+    setState(() {
+      bgColors[index] = color;
+    });
   }
 }
