45e5ef9e53a04d7d580dfe0167c08bb16a2d9921
faithcredit <honest.dedicate@gmail.com>
2023-06-30 14:30:07 +0900

    Interacting with desktop menus

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide MenuBar;
 import '../models/book.dart';
 import '../data/http_helper.dart';
+import 'package:menu_bar/menu_bar.dart';
 
 class BookListScreen extends StatefulWidget {
   const BookListScreen({super.key});
+
   @override
   State<BookListScreen> createState() => _BookListScreenState();
 }
@@ -36,35 +38,82 @@ class _BookListScreenState extends State<BookListScreen> {
     } else {
       isLargeScreen = false;
     }
-    return Scaffold(
-        appBar: AppBar(title: const Text('Books')),
-        body: GridView.count(
-            childAspectRatio: isLargeScreen ? 8 : 5,
-            crossAxisCount: isLargeScreen ? 2 : 1,
-            children: List.generate(books.length, (index) {
-              return GestureDetector(
-                onTap: () => setColor(Colors.lightBlue, index),
-                onSecondaryTap: () => setColor(Colors.white, index),
-                onLongPress: () => setColor(Colors.white, index),
-                child: ColoredBox(
-                  color: bgColors.isNotEmpty ? bgColors[index] : Colors.white,
-                  child: ListTile(
-                    title: Text(books[index].title),
-                    subtitle: Text(books[index].authors),
-                    leading: CircleAvatar(
-                      backgroundImage: (books[index].thumbnail) == ''
-                          ? null
-                          : NetworkImage(books[index].thumbnail),
+    List<BarButton> myMenu = buildBarButton();
+
+    return MenuBarWidget(
+      barButtons: myMenu,
+      child: Scaffold(
+          appBar: AppBar(title: const Text('Books')),
+          body: GridView.count(
+              childAspectRatio: isLargeScreen ? 8 : 5,
+              crossAxisCount: isLargeScreen ? 2 : 1,
+              children: List.generate(books.length, (index) {
+                return GestureDetector(
+                  onTap: () => setColor(Colors.lightBlue, index),
+                  onSecondaryTap: () => setColor(Colors.white, index),
+                  onLongPress: () => setColor(Colors.white, index),
+                  child: ColoredBox(
+                    color: bgColors.isNotEmpty ? bgColors[index] : Colors.white,
+                    child: ListTile(
+                      title: Text(books[index].title),
+                      subtitle: Text(books[index].authors),
+                      leading: CircleAvatar(
+                        backgroundImage: (books[index].thumbnail) == ''
+                            ? null
+                            : NetworkImage(books[index].thumbnail),
+                      ),
                     ),
                   ),
-                ),
-              );
-            })));
+                );
+              }))),
+    );
   }
 
   void setColor(Color color, int index) {
     setState(() {
       bgColors[index] = color;
     });
+  }
+
+  void updateBooks(String key) {
+    helper.getBooks(key).then((List<Book> value) {
+      List<Color> newBgColors =
+          List.generate(value.length, (index) => Colors.white);
+      setState(() {
+        books = value;
+        bgColors = newBgColors;
+      });
+    });
+  }
+
+  List<BarButton> buildBarButton() {
+    return [
+      BarButton(
+        text: const Text(
+          'Search',
+          style: TextStyle(color: Colors.white),
+        ),
+        submenu: SubMenu(menuItems: [
+          MenuButton(
+            onTap: () {
+              updateBooks('Flutter');
+            },
+            text: const Text('Flutter'),
+          ),
+          MenuButton(
+            onTap: () {
+              updateBooks('C#');
+            },
+            text: const Text('C#'),
+          ),
+          MenuButton(
+            onTap: () {
+              updateBooks('JavaScript');
+            },
+            text: const Text('JavaScript'),
+          ),
+        ]),
+      )
+    ];
   }
 }
